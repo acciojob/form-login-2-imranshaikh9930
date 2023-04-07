fa4279c516c3f2b5f33b7ca8500a8263240844d0
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-04-07 23:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const fname = document.querySelector("#fname");
+const lname = document.querySelector("#lname");
+const pnumber = document.querySelector("#pnumber");
+const email  =document.querySelector("#emailId");
+
+const btn = document.querySelector(".button");
+btn.addEventListener("click",()=>{
+	alert(`First Name: ${fname.value}\nLast Name: ${lname.value}\nPhone Number: ${pnumber.value}\nEmail ID: ${email.value}`);
+	// alert("First Name: "+lname.value);
+})
